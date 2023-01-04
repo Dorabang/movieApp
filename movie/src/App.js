e7f21630f3d360907bfcfc04ae617344912f5329
Dora_bang <psa1195@naver.com>
2023-01-04 20:25:59 +0900

    coins tracker

--- movie/src/App.js
+++ movie/src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  // 비어있는 배열을 기본값으로 주어, 처음 새로고침되었을때(API 정보를 받아올때) 발생하는 오류를 방지함
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -13,15 +14,18 @@ function App() {
   }, []);
   return (
     <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin) => (
-          <li>
-            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-          </li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
