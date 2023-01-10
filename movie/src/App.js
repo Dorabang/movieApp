c8c7a7635068fffe5b375f46b2c4334ca20bb403
Dora_bang <39180932+Dorabang@users.noreply.github.com>
2023-01-10 17:04:49 +0900

    Change the cost

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,11 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
   // 비어있는 배열을 기본값으로 주어, 처음 새로고침되었을때(API 정보를 받아올때) 발생하는 오류를 방지함
+  const [cost, setCost] = useState(1);
+  const [need, setNeed] = useState(1);
+  const onChange = (event) => {
+    setCost(event.target.value);
+    setNeed(1);
+  };
+  const handleInput = (event) => setNeed(event.target.value);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
+    fetch('https://api.coinpaprika.com/v1/tickers')
       .then((response) => response.json())
       .then((json) => {
         setCoins(json);
@@ -14,18 +21,35 @@ function App() {
   }, []);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      <h1>The Coins! {loading ? '' : `(${coins.length})`}</h1>
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
+        <select onChange={onChange}>
           {coins.map((coin) => (
-            <option key={coin.id}>
+            <option
+              key={coin.id}
+              value={coin.quotes.USD.price}
+              id={coin.symbol}
+              symbol={coin.symbol}
+            >
               {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-            </option> // coin print
+            </option>
           ))}
         </select>
       )}
+
+      <hr />
+      <h2>Press coin 💰</h2>
+      <div>
+        <input
+          type='number'
+          value={need}
+          onChange={handleInput}
+          placeholder='dollor'
+        ></input>
+        <p>You can get {need / cost}</p>
+      </div>
     </div>
   );
 }
